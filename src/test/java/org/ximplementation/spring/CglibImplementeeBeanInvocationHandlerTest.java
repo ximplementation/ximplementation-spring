44063315174294f6b7a63891b57a0f5ca1a7d31e
earthangry <earthangry@gmail.com>
2016-11-29 12:44:00 +0800

    Add class doc

--- src/test/java/org/ximplementation/spring/CglibImplementeeBeanInvocationHandlerTest.java
+++ src/test/java/org/ximplementation/spring/CglibImplementeeBeanInvocationHandlerTest.java
@@ -1,3 +1,17 @@
+/**
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *  
+  * 	http://www.apache.org/licenses/LICENSE-2.0
+  *  
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License. 
+  */
+
 package org.ximplementation.spring;
 
 import java.lang.reflect.Method;
@@ -12,6 +26,13 @@ import org.ximplementation.support.ImplementationResolver;
 import org.ximplementation.support.ImplementorBeanFactory;
 import org.ximplementation.support.SimpleImplementorBeanFactory;
 
+/**
+ * {@linkplain CglibImplementeeBeanInvocationHandler} unit tests.
+ * 
+ * @author earthangry@gmail.com
+ * @date 2016-11-2
+ *
+ */
 public class CglibImplementeeBeanInvocationHandlerTest
 {
 	private ImplementationResolver implementationResolver;
